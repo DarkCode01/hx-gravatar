fd0f64109a4b426a962f6797560509acdc68ac77
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-09-14 16:56:07 -0400

    feat: implemented tests

--- test/ex_gravatar_test.exs
+++ test/ex_gravatar_test.exs
@@ -1,8 +1,39 @@
 defmodule ExGravatarTest do
   use ExUnit.Case
-  doctest ExGravatar
 
-  test "greets the world" do
-    assert ExGravatar.hello() == :world
+  test "generate hash" do
+    hash = ExGravatar.generate_hash("example@example.com")
+    
+    assert [49, 197, 84, 60, 23, 52, 210, 92, 114, 6, 245, 253, 89, 21, 37, 208, 41, 91,
+    236, 111, 232, 79, 248, 47, 148, 106, 52, 254, 151, 10, 30, 102] == hash
+  end
+
+  test "picker color" do
+    color = ExGravatar.picker_color()
+
+    assert is_atom(color) == true
+  end
+
+  test "creation of grid or table" do
+    hash = ExGravatar.generate_hash("example@example.com")
+    grid = ExGravatar.generate_grid(hash)
+
+    assert length(grid) == 54
+    assert is_list(grid) == true
+  end
+
+  test "creation of row" do
+    row = ExGravatar.create_row([1, 2, 3, 4, 5])\
+
+    assert is_list(row) == true
+    assert row == [1, 2, 3, 4, 5, 4, 3, 2, 1]
+  end
+
+  test "print element" do
+    element_color = ExGravatar.print_element(true, :red)
+    element_without_color = ExGravatar.print_element(false, nil)
+
+    assert element_without_color == "█"
+    assert element_color == [[[[] | "\e[31m"], "█"] | "\e[0m"]
   end
 end
