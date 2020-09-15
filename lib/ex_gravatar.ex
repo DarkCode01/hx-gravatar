b078c7369b756b8c136f428172e991ff9104d3d6
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-09-14 21:05:58 -0400

    feat: implement docs and test

--- lib/ex_gravatar.ex
+++ lib/ex_gravatar.ex
@@ -5,42 +5,109 @@ defmodule ExGravatar do
 
   alias IO.ANSI, as: Ansi
 
-  @colors [:red, :yellow, :blue, :black]
-
   @doc """
   Display the gravatar acsii image
 
   ## Examples
 
       iex> ExGravatar.generate("example@example.com")
-      █████████
-      █████████
-      █████████
-      █████████
-      █████████
-      █████████
-      :ok
+      [
+        [
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"]
+        ],
+        [
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"]
+        ],
+        [
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"]
+        ],
+        [
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"]
+        ],
+        [
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"]
+        ],
+        [
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[31m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"],
+          [[[[] | "\e[37m"], "█"] | "\e[0m"]
+        ]
+      ]
 
   """
+  @spec generate(String.t()) :: Enumerable.t()
   def generate(input) do
-    color = picker_color()
-
     input
     |> generate_hash
     |> generate_grid
     |> Enum.map(& rem(&1, 2) == 1)
-    |> Enum.map(& print_element(&1, color))
+    |> Enum.map(&print_element/1)
     |> Enum.chunk_every(9, 9, :discard)
-    |> Enum.each(& IO.puts(&1))
   end
 
-  @doc false
+  @doc """
+  Generate sha256 hash to create grid.
+
+  ## Examples
+      iex> ExGravatar.generate_hash("example@example.com")
+      [49, 197, 84, 60, 23, 52, 210, 92, 114, 6, 245, 253, 89, 21, 37, 208, 41, 91, 236, 111, 232, 79, 248, 47, 148, 106, 52, 254, 151, 10, 30, 102]
+  """
+  @spec generate(String.t()) :: Enumerable.t()
   def generate_hash(info) do
     :crypto.hash(:sha256, info)
     |> :binary.bin_to_list
   end
 
-  @doc false
+  @doc """
+  Generate grid/table of image gravatar.
+  """
+  @spec generate(Enumerable.t()) :: Enumerable.t()
   def generate_grid(list) do
     list
     |> Enum.chunk_every(5, 5, :discard)
@@ -48,7 +115,14 @@ defmodule ExGravatar do
     |> List.flatten
   end
 
-  @doc false
+  @doc """
+  Implemente mirror to elements of a row
+
+  ## Examples
+      iex> ExGravatar.create_row([1, 2, 3, 4, 5])
+      [1, 2, 3, 4, 5, 4, 3, 2, 1]
+  """
+  @spec generate(Enumerable.t()) :: Enumerable.t()
   def create_row(list) do
     list
     |> Enum.chunk_every(4, 4, :discard)
@@ -56,12 +130,25 @@ defmodule ExGravatar do
     |> Enum.concat(Enum.reverse(list))
   end
 
-  @doc false
-  def print_element(false, _color), do: "█"
-  def print_element(true, color), do: Ansi.format([color, "█"])
-
-  @doc false
-  def picker_color do
-    Enum.random(@colors)
+  @doc """
+  Impure function to display gravatar.
+  """
+  @spec generate(Enumerable.t()) :: Atom.t()
+  def print_gravatar(gravatar) do
+    gravatar
+    |> Enum.each(&IO.puts/1)
   end
+
+  @doc """
+  Replace all odd and not-odd cell by colors.
+
+  ## Examples
+      iex> ExGravatar.print_element(true)
+      [[[[] | "\e[31m"], "█"] | "\e[0m"]
+      iex> ExGravatar.print_element(false)
+      [[[[] | "\e[37m"], "█"] | "\e[0m"]
+  """
+  @spec generate(Atom.t()) :: Enumerable.t()
+  def print_element(false), do: Ansi.format([:white, "█"])
+  def print_element(true), do: Ansi.format([:red, "█"])
 end
--- test/ex_gravatar_test.exs
+++ test/ex_gravatar_test.exs
@@ -1,17 +1,13 @@
 defmodule ExGravatarTest do
   use ExUnit.Case
 
+  doctest ExGravatar
+
   test "generate hash" do
     hash = ExGravatar.generate_hash("example@example.com")
     
     assert [49, 197, 84, 60, 23, 52, 210, 92, 114, 6, 245, 253, 89, 21, 37, 208, 41, 91,
     236, 111, 232, 79, 248, 47, 148, 106, 52, 254, 151, 10, 30, 102] == hash
-  end
-
-  test "picker color" do
-    color = ExGravatar.picker_color()
-
-    assert is_atom(color) == true
   end
 
   test "creation of grid or table" do
@@ -23,17 +19,19 @@ defmodule ExGravatarTest do
   end
 
   test "creation of row" do
-    row = ExGravatar.create_row([1, 2, 3, 4, 5])\
+    row = ExGravatar.create_row([1, 2, 3, 4, 5])
 
     assert is_list(row) == true
     assert row == [1, 2, 3, 4, 5, 4, 3, 2, 1]
   end
 
   test "print element" do
-    element_color = ExGravatar.print_element(true, :red)
-    element_without_color = ExGravatar.print_element(false, nil)
+    element_color = ExGravatar.print_element(true)
+    element_without_color = ExGravatar.print_element(false)
 
-    assert element_without_color == "█"
+    assert is_list(element_color)
+    assert is_list(element_without_color)
+    assert element_without_color == [[[[] | "\e[37m"], "█"] | "\e[0m"]
     assert element_color == [[[[] | "\e[31m"], "█"] | "\e[0m"]
   end
 end
